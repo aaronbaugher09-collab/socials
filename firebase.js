2d751c64d23be73d7bc726f815644875429216a0
aaronbaugher09-collab <aaronbaugher09@gmail.com>
2026-08-03 15:33:44 -0500

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,27 @@
+// Import Firebase SDKs
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
+
+// Replace this with YOUR Firebase config
+const firebaseConfig = {
+
+    apiKey: "",
+
+    authDomain: "",
+
+    projectId: "",
+
+    storageBucket: "",
+
+    messagingSenderId: "",
+
+    appId: ""
+
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+
+export const db = getFirestore(app);
