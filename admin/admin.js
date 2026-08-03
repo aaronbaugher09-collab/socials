039fabdcf854bcb89dc171bfc5e27ad41e3a2dda
aaronbaugher09-collab <aaronbaugher09@gmail.com>
2026-08-03 15:36:07 -0500

    Add Firebase login functionality
    Implement login functionality using Firebase authentication.

--- admin/admin.js
+++ admin/admin.js
@@ -1,7 +1,41 @@
-document
-.getElementById("loginButton")
-.addEventListener("click", ()=>{
+import { auth } from "../firebase.js";
 
-    alert("Login system coming next!");
+import {
 
-});
+    signInWithEmailAndPassword
+
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+
+const loginButton = document.getElementById("loginButton");
+
+loginButton.addEventListener("click", login);
+
+async function login(){
+
+    const email = document.getElementById("email").value;
+
+    const password = document.getElementById("password").value;
+
+    const error = document.getElementById("error");
+
+    error.textContent = "";
+
+    try{
+
+        await signInWithEmailAndPassword(
+            auth,
+            email,
+            password
+        );
+
+        window.location.href="dashboard.html";
+
+    }
+
+    catch(e){
+
+        error.textContent=e.message;
+
+    }
+
+}
